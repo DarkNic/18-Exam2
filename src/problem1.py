d1039fd0bd49380fb49c4f1ba877e84d6b0c0faa
bohnernj <bohnernj@rose-hulman.edu>
2018-10-09 20:52:58 -0400

    K

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 2, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Nicolas Bohner.  October 2018.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -48,10 +48,10 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_problem1a()
-    # run_test_problem1b()
-    # run_test_problem1c()
-    # run_test_problem1d()
+    run_test_problem1a()
+    run_test_problem1b()
+    run_test_problem1c()
+    run_test_problem1d()
 
 
 def run_test_problem1a():
@@ -218,9 +218,13 @@ def problem1a(strings):
       :rtype: list of int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    num = []
+    for k in range(len(strings)):
+        num.append(len(strings[k]))
+    return num
 
 
 def run_test_problem1b():
@@ -391,9 +395,14 @@ def problem1b(strings):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # Done: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    count = 0
+    for k in range(len(strings)):
+        if is_prime(len(strings[k])) == True: #In future tests, can I use 'is' instead of ==
+            count = count+1
+    return count
 
 
 def run_test_problem1c():
@@ -587,11 +596,17 @@ def problem1c(strings):
       :rtype: bool
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     #          Tests have been written for you (above).
     # See  IMPORTANT  note before the DEF line of this function.
     # -------------------------------------------------------------------------
+    if is_prime(problem1b(strings)) == True:
+        return True
+    return False
 
+        # Is it possible to have the additional instructions with the todo?
+        # I nearly missed it and only checked because it seemed strange
+        # if I didn't use the above function
 
 def run_test_problem1d():
     """ Tests the   problem1d   function. """
@@ -774,10 +789,13 @@ def problem1d(strings):
       :rtype: str | -1
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # Done: 5. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    for k in range(len(strings)):
+        if is_prime(len(strings[k])) == True:
+            return strings[k]
+    return -1
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
